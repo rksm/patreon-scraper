e36b8cf3bb6bafeaa7151c46552c002830199e69
Robert Krahn <robert.krahn@gmail.com>
2019-11-28 16:32:29 +0100

    added posted file data to rendering

--- render_data.ts
+++ render_data.ts
@@ -11,7 +11,7 @@ function main() {
   let html = `
 <style>
 .date {}
-.count {
+.count, .post_file {
 font-weight: bold;
 }
 img {
@@ -26,13 +26,16 @@ max-height: 400px;
     JSON.parse(readFileSync(`./${dataDir}/data.json`).toString());
 
   for (const post of data) {
-    const {content, title, like_count, url, comment_count, published_at} =
+    const {content, title, like_count, url, comment_count, published_at, post_file} =
       post.attributes;
     html += `<h2><a href="${url}">${title}</a></h2>
 <div class="date">${published_at}</div>
 <div class="count">likes: ${like_count}</div>
-<div class="count">comments: ${comment_count}</div>
-<div>
+<div class="count">comments: ${comment_count}</div>\n`;
+    if (post_file) {
+      html += `<div class="post_file">linked file: <a href="${post_file.url}">${post_file.name}</a></div>\n`;
+    }
+    html += `<div>
 ${content}
 </div>
 <hr />
