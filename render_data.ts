bb52d6491855069fb6cd5f29adf03db6126546d0
Robert Krahn <robert.krahn@gmail.com>
2019-11-28 16:38:33 +0100

    post_file image handling

--- render_data.ts
+++ render_data.ts
@@ -2,7 +2,15 @@ import {readFileSync, writeFileSync} from 'fs';
 import {resolve} from 'path';
 
 import {formatDate, parseArgs} from './util';
-import {DataEntity} from './PostDataInterface';
+import {DataEntity, PostFile} from './PostDataInterface';
+
+function renderPostFile(postFile: PostFile): String {
+  const isImage = !!postFile.url.match(/\.(jpe?g|png|gif|bmp)/);
+  if (isImage) {
+    return `<div><a href="${postFile.url}"><img src="${postFile.url}"/></a></div>\n`;
+  }
+  return `<div class="post_file">linked file: <a href="${postFile.url}">${postFile.name}</a></div>\n`;
+}
 
 function main() {
   const {dataDir} = parseArgs(process.argv.slice(2));
@@ -33,7 +41,7 @@ max-height: 400px;
 <div class="count">likes: ${like_count}</div>
 <div class="count">comments: ${comment_count}</div>\n`;
     if (post_file) {
-      html += `<div class="post_file">linked file: <a href="${post_file.url}">${post_file.name}</a></div>\n`;
+      html += renderPostFile(post_file);
     }
     html += `<div>
 ${content}
