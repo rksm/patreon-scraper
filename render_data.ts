29eb5c6ac03a2d4107ad228d0985bfb674b4193b
cornzz <39997278+cornzz@users.noreply.github.com>
2021-11-28 20:21:00 +0100

    add support for embedded creator content

--- render_data.ts
+++ render_data.ts
@@ -5,54 +5,68 @@ import { formatDate, parseArgs } from './util';
 import { DataEntity, PostFile } from './PostDataInterface';
 
 function renderPostFile(postFile: PostFile): String {
-  const isImage = !!postFile.url.match(/\.(jpe?g|png|gif|bmp)/);
-  if (isImage) {
-    return `<div><a href="${postFile.url}"><img src="${postFile.url}"/></a></div>\n`;
-  }
-  return `<div class="post_file">linked file: <a href="${postFile.url}">${postFile.name}</a></div>\n`;
+    const isImage = !!postFile.url.match(/\.(jpe?g|png|gif|bmp)/);
+    if (isImage) {
+        return `<div><a href="${postFile.url}"><img src="${postFile.url}"/></a></div>\n`;
+    }
+    return `<div class="post_file">linked file: <a href="${postFile.url}">${postFile.name}</a></div>\n`;
 }
 
 function main() {
-  const { dataDir } = parseArgs(process.argv.slice(2));
+    const { dataDir } = parseArgs(process.argv.slice(2));
 
-  const htmlFile = `./${dataDir}/${formatDate()}_index.html`;
-  let html = `
-<style>
-.date {}
-.count, .post_file {
-font-weight: bold;
-}
-img {
-max-width: 500px;
-max-height: 400px;
-}
-</style>
+    const htmlFile = `./${dataDir}/${formatDate()}_index.html`;
+    let html = `
+        <style>
+            .date {}
+            .count, .post_file {
+                font-weight: bold;
+            }
+            img {
+                max-width: 500px;
+                max-height: 400px;
+            }
+            iframe {
+                width: 720px;
+                height: 480px;
+            }
+        </style>
+        <script>
+            function loadVideo(index) {
+                let temp = document.getElementById(\`video-\${index}\`);
+                temp.parentNode.appendChild(temp.content.cloneNode(true));
+            }
+        </script>
+    `;
 
-`;
+    const data: DataEntity[] =
+        JSON.parse(readFileSync(`./${dataDir}/data.json`).toString());
 
-  const data: DataEntity[] =
-    JSON.parse(readFileSync(`./${dataDir}/data.json`).toString());
+    data.forEach((post, index) => {
+        const { content, title, like_count, url, comment_count, published_at, post_file, embed } = post.attributes;
+        html += `<h2><a href="${url}">${title}</a></h2>
+        <div class="date">${published_at}</div>
+        <div class="count">likes: ${like_count}</div>
+        <div class="count">comments: ${comment_count}</div>\n`;
+        if (post_file) {
+            html += renderPostFile(post_file);
+        }
+        if (embed) {
+            let video = embed.html.replace('"//', '"https://');
+            html += `<div>
+            <button onclick="loadVideo(${index})">Load video</button>
+            <template class="video" id="video-${index}">${video}</template>
+            </div>`;
+        }
+        html += `<div>
+        ${content}
+        </div>
+        <hr />
+        `;
+    });
 
-  for (const post of data) {
-    const { content, title, like_count, url, comment_count, published_at, post_file } =
-      post.attributes;
-    html += `<h2><a href="${url}">${title}</a></h2>
-<div class="date">${published_at}</div>
-<div class="count">likes: ${like_count}</div>
-<div class="count">comments: ${comment_count}</div>\n`;
-    if (post_file) {
-      html += renderPostFile(post_file);
-    }
-    html += `<div>
-${content}
-</div>
-<hr />
-`;
-  }
-
-
-  writeFileSync(htmlFile, html);
-  console.log(`file://${resolve(htmlFile)}`);
+    writeFileSync(htmlFile, html);
+    console.log(`file://${resolve(htmlFile)}`);
 }
 
 main();
